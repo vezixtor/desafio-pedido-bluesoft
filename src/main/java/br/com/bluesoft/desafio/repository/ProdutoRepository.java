45f288b240bfcbc32678a712209831e2f7314cf7
Everson <everson___@hotmail.com>
2019-09-23 21:52:23 -0300

    add repository de produto para encontrar por gtin

--- src/main/java/br/com/bluesoft/desafio/repository/ProdutoRepository.java
+++ src/main/java/br/com/bluesoft/desafio/repository/ProdutoRepository.java
@@ -1,8 +1,9 @@
 package br.com.bluesoft.desafio.repository;
 
+import br.com.bluesoft.desafio.model.Produto;
 import org.springframework.data.repository.CrudRepository;
 
-import br.com.bluesoft.desafio.model.Produto;
-
 public interface ProdutoRepository extends CrudRepository<Produto, String> {
+
+    Produto findByGtin(String gtin);
 }
